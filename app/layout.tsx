bda62aeb62e4443bcd174c1c9b31a9764455182f
Rommanowski <jakub.romanowski17@gmail.com>
2026-05-04 22:22:52 +0200

    chore: change websites title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 import "./globals.css";
 
 
+export const metadata = {
+    title: "listing page",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html
       lang="en"
